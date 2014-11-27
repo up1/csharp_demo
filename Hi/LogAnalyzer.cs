f9776208c88968ea4d4e2150508de3d7f930cd96
somkiat <somkiat.p@gmail.com>
2014-11-27 18:29:45 +0700

    Add new example of LogAnalyzer

---
+++ Hi/LogAnalyzer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+namespace Hi
+{
+	public class LogAnalyzer
+	{
+		public LogAnalyzer ()
+		{
+		}
+
+		public bool isValidFileName(String filename) {
+			string line;
+			ArrayList extensions = new ArrayList ();
+			StreamReader file = new StreamReader(@"/Users/somkiat/Projects/Hi/Hi/extension.txt");
+			while( (line = file.ReadLine())  != null) {
+				if(!String.IsNullOrEmpty(line)) {
+					extensions.Add (line);
+				}
+			}
+			file.Close ();
+
+			foreach(String extension in extensions) {
+				if(filename.EndsWith(extension)) {
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+}
+
---
+++ HiTest/LogAnalyzerTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using Hi;
+
+namespace HiTest
+{
+	[TestFixture]
+	public class LogAnalyzerTest
+	{
+
+		[Test]
+		public void isValidFilenameWithLowerCase_Return_True() {
+			LogAnalyzer  analyzer = new LogAnalyzer();
+			bool result = analyzer.isValidFileName ("test.txt");
+			Assert.IsTrue (result, "Filename should be valid");
+		}
+
+		[Test]
+		public void isValidFilenameWithUpperCase_Return_True() {
+			LogAnalyzer  analyzer = new LogAnalyzer();
+			bool result = analyzer.isValidFileName ("test.TXT");
+			Assert.IsTrue (result, "Filename should be valid");
+		}
+
+	}
+}
+
